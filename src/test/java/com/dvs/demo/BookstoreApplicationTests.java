9f1e41deea879ce2c77aebca47c1d76920b74724
dvsyd <dvsyd@NITRO5-DVS>
2021-08-31 14:37:23 -0300

    Versão Inicial

---
+++ src/test/java/com/dvs/demo/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
